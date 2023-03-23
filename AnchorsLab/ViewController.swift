cc4499745692b6c660428aab71e55bdcf1a2f27b
Edwin Cardenas <eacardenase@unal.edu.co>
2023-03-23 10:50:33 -0500

    feat: add elements to the view with auto layout

--- AnchorsLab/ViewController.swift
+++ AnchorsLab/ViewController.swift
@@ -11,9 +11,111 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
     }
-
-
 }
 
+extension ViewController {
+    func setupViews() {
+        let upperLeftLabel = makeLabel(withText: "upperLeft")
+        let upperRightLabel = makeLabel(withText: "upperRight")
+        let bottomLeftLabel = makeSecondaryLabel(withText: "bottomLeft")
+        let bottomRightButton = makeButton(withText: "Pay Bill")
+        let redView = makeView()
+        
+        view.addSubview(upperLeftLabel)
+        view.addSubview(upperRightLabel)
+        view.addSubview(bottomLeftLabel)
+        view.addSubview(bottomRightButton)
+        view.addSubview(redView)
+        
+        // upperLeftLabel
+        NSLayoutConstraint.activate([
+            upperLeftLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            upperLeftLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8)
+        ])
+        
+        // upperRightLabel
+        NSLayoutConstraint.activate([
+            upperRightLabel.topAnchor.constraint(equalTo: upperLeftLabel.topAnchor),
+            upperRightLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8)
+//            view.trailingAnchor.constraint(equalTo: upperRightLabel.trailingAnchor, constant: 8)
+        ])
+        
+        // bottomLeftLabel
+        NSLayoutConstraint.activate([
+            bottomLeftLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            bottomLeftLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8)
+        ])
+        
+        // bottomRightButton
+        NSLayoutConstraint.activate([
+            bottomRightButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            bottomRightButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8)
+        ])
+        
+        // redView
+        NSLayoutConstraint.activate([
+            redView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            redView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            // Option 1: Size explicitly
+            
+//            redView.heightAnchor.constraint(equalToConstant: 50),
+//            redView.widthAnchor.constraint(equalToConstant: 100)
+            
+            // Option 2: Size dynamically (pinning)
+            
+            // width
+            redView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            redView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            // height
+            redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            redView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+            
+        ])
+    }
+    
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = .yellow
+        
+        return label
+    }
+    
+    func makeSecondaryLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .yellow
+        label.text = text
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .gray
+        
+        return label
+    }
+    
+    func makeButton(withText text: String) -> UIButton {
+        let button = UIButton()
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(text, for: .normal)
+        button.backgroundColor = .blue
+        
+        return button
+    }
+    
+    func makeView() -> UIView {
+        let view = UIView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        
+        return view
+    }
+}
